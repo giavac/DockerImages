dd1e166a540164f4feeabc1588e46f7cf6efc625
Alfonso Pinto <alfonso.pinto@gmail.com>
2015-04-21 11:38:41 -0400

    Fixed issue installing fpm Added cronie as dependency for createrepo Ensure cache folder for repository exists.

--- yumrepo/yumrepo.pp
+++ yumrepo/yumrepo.pp
@@ -2,27 +2,28 @@ include epel
 include cpan
 
 package { 'rrdtool-perl':
-	ensure => present,
+  ensure => present,
 }->
 package { 'perl-ExtUtils-MakeMaker':
-	ensure => present,	
+  ensure => present,	
 }->
-package { 'fpm':
-	ensure => present,
-	provider => gem
-}
-
 cpan { "App::cpanminus":
   ensure  => present,
   require => Class['::cpan'],
   force   => true,
 }
 
-class { 'apache': }
+class { 'apache': }->
 
 apache::vhost { 'first.example.com':
   port    => '80',
   docroot => '/var/www/html/puppetrepo',
+}->
+package { 'cronie':
+  ensure => present,
+}->
+file { '/var/cache/puppetrepo/':
+  ensure => 'directory',
 }->
 createrepo { 'yumrepo':
   repository_dir => '/var/www/html/puppetrepo/el-6.5',
